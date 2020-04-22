833c5660a82289e274f4578a8d14854e1d7ed891
cirrus42 <tim@cirrus42.co.uk>
2020-04-22 21:55:12 +0100

    studioHandler - isDuplicate Method studiohandelr - SaveStudio Method

--- src/FrontEnd/Handlers/StudioHandler.cs
+++ src/FrontEnd/Handlers/StudioHandler.cs
@@ -9,12 +9,26 @@ namespace FilmReference.FrontEnd.Handlers
 {
     public class StudioHandler : IStudioHandler
     {
-        private readonly IGenericRepository<Studio> _genreRepository;
+        private readonly IGenericRepository<Studio> _studioRepository;
 
         public StudioHandler(IGenericRepository<Studio> personRepository) =>
-            _genreRepository = personRepository;
+            _studioRepository = personRepository;
 
         public async Task<IEnumerable<Studio>> GetStudios() =>
-            (await _genreRepository.GetAll()).OrderBy(genre => genre.Name);
+            (await _studioRepository.GetAll()).OrderBy(genre => genre.Name);
+
+        public async Task<bool> IsDuplicate(Studio studio)
+        {
+            var duplicates = (await _studioRepository.GetWhere(s =>
+                s.Name.ToLower().Replace(" ", "") == studio.Name.ToLower().Replace(" ", ""))).ToList();
+
+            return duplicates.Any() && duplicates.Any(s => s.StudioId != studio.StudioId);
+        }
+
+        public async Task SaveStudio(Studio studio)
+        {
+            await _studioRepository.Add(studio);
+            await _studioRepository.Save();
+        }
     }
 }
